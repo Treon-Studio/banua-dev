11301515b28de8e3a70c09fc5b71a82cc732f2e1
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2026-04-06 11:01:23 +0700

    feat: update code]

--- homepage/astro.config.mjs
+++ homepage/astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   vite: {
     plugins: [tailwindcss()],
   },
